36b208dfce284d1adc7c64a570048d8b1622f91d
Mykola Avdieiev <ronsaval@gmail.com>
2019-03-17 17:08:48 +0200

    New test added

--- src/test/java/PetStoreTests.java
+++ src/test/java/PetStoreTests.java
@@ -2,24 +2,32 @@ import org.testng.annotations.Test;
 
 public class PetStoreTests {
 
-    final PetEndpoint PET_ENDPOINT = new PetEndpoint(); //new static variable from PetEndpoint
+    final static PetEndpoint PET_ENDPOINT = new PetEndpoint(); //new constant variable from PetEndpoint
 
     @Test
     public void getPetById200() {
-        PET_ENDPOINT.getPetById(1)
+        PET_ENDPOINT
+                .getPetById(1)
                 .then()
                 .statusCode(200);
-
 
     }
 
     @Test
     public void getPetById404() {
-        PET_ENDPOINT.getPetById(2)
+        PET_ENDPOINT
+                .getPetById(0)
                 .then()
                 .statusCode(404);
 
     }
 
+    @Test
+    public void getPetByStatus(){
+        PET_ENDPOINT
+                .getPetByStatus("available")
+                .then()
+                .statusCode(200);
+    }
 
 }
