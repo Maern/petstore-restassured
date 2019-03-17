ffb0c5cb52f899b22fe874ce191944149e91c658
Mykola Avdieiev <ronsaval@gmail.com>
2019-03-17 14:04:28 +0200

    adding constant

---
+++ src/test/java/Config.java
@@ -0,0 +1,3 @@
+public class Config {
+    public final String PET_STORE_BASE_HOST = "https://petstore.swagger.io/v2";
+}
---
+++ src/test/java/PetStoreTests.java
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class PetStoreTests {
+    @Test
+    public void getPetById200(){
+        RestAssured.given().baseUri("https://petstore.swagger.io/v2/pet/1").get().then().statusCode(200);
+                //.and().extract().body();
+
+    }
+
+    @Test
+    public void getPetById404(){
+        RestAssured.given().baseUri("https://petstore.swagger.io/v2/pet/0").get().then().statusCode(404);
+
+    }
+}
