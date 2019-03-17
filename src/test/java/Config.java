7a3e834d3156bdb9308e1fb9751d4b61cf06f904
Mykola Avdieiev <ronsaval@gmail.com>
2019-03-17 14:06:36 +0200

    constant static, name changed

--- src/test/java/Config.java
+++ src/test/java/Config.java
@@ -1,3 +1,3 @@
 public class Config {
-    public final String PET_STORE_BASE_HOST = "https://petstore.swagger.io/v2";
+    public static final String PET_STORE_BASE_URI = "https://petstore.swagger.io/v2";
 }
