cf6615c62dde3c2648221fbe1af1ae451c91b381
Mykola Avdieiev <ronsaval@gmail.com>
2019-03-17 16:05:46 +0200

    tests updated with new method

--- src/test/java/PetStoreTests.java
+++ src/test/java/PetStoreTests.java
@@ -1,15 +1,13 @@
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
 public class PetStoreTests {
     @Test
-    public void getPetById200(){
-        RestAssured
-                .given()
+    public void getPetById200() {
+        baseSpecification()
                 .basePath(Config.GET_PET_ENDPOINT)
-                .pathParam("id",1)
+                .pathParam("id", 1)
                 .get()
                 .then()
                 .statusCode(200);
@@ -18,15 +16,17 @@ public class PetStoreTests {
     }
 
     @Test
-    public void getPetById404(){
-        RestAssured.given().baseUri(Config.PET_STORE_BASE_URI)
-                .baseUri("/pet/0")
-                .log().uri()
-                .then().statusCode(404);
+    public void getPetById404() {
+        baseSpecification()
+                .basePath(Config.GET_PET_ENDPOINT)
+                .pathParam("id", 0)
+                .get()
+                .then()
+                .statusCode(404);
 
     }
 
-    private RequestSpecification baseSpecification(){
+    private RequestSpecification baseSpecification() {
         return RestAssured.given()
                 .baseUri(Config.PET_STORE_BASE_URI)
                 .log().uri();
